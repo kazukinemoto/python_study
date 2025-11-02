d73110aa08222beac56dbbb49fd6b34c48d9d34e
kazuki <redrows.kazu@gmail.com>
2025-11-02 19:52:13 +0900

    example

---
+++ example.py
@@ -0,0 +1,43 @@
+
+# 1. example.pyを作成して「hello world」と出力してください。
+
+print("hello world")
+ 
+# 2. greet関数を実装し、「こんにちは」と出力するようにしてください。関数を呼び出して、実際に出力されることを確認してください。
+
+def greet():
+    print("こんにちは")
+
+greet()
+
+# 3. nameを引数に取り、「私の名前は{name}です」と出力するprint_name関数を実装し、関数を呼び出して動作を確認してください。
+
+def print_name(name):
+    n = name
+    print(f"私の名前は{n}です")
+
+print_name("根本")
+
+
+# 4. 「おはようございます」という文字列を戻り値として返すget_greet関数を実装し、戻り値をprint関数で出力してください。
+
+def get_greet():
+    g = "おはようございます"
+    return g
+
+hello = get_greet()
+
+print(hello)
+
+
+# 5. a, bを引数に取り、その足し算の結果を戻り値として返すadd関数を実装し、関数を呼び出して結果をprint関数で出力してください。
+
+def add(a, b):
+    sum = a + b
+    return sum
+
+x = add(5,21)
+
+print(x)
+
+# 6. example.pyをリポジトリにコミットし、pushしてください。
